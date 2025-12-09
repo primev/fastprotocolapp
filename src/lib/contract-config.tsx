65a4bc2d75e410a1e6eb91e14c4dd5298360dd34
Jason Schwarz <jason.c.schwarz@gmail.com>
2025-12-09 19:20:39 -0400

    wip: contract config

---
+++ src/lib/contract-config.tsx
@@ -0,0 +1,9 @@
+export const CONTRACT_ABI = [
+    { "inputs": [], "name": "mint", "outputs": [], "stateMutability": "payable", "type": "function" },
+    { "inputs": [{ "internalType": "address", "name": "owner", "type": "address" }], "name": "balanceOf", "outputs": [{ "internalType": "uint256", "name": "", "type": "uint256" }], "stateMutability": "view", "type": "function" }
+]
+
+export const CONTRACT_ADDRESS = "";
+export const EXPLORER_URL = "https://etherscan.io/address/";
+
+
