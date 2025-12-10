19235d7f69792fc68f0c66e83c8ab383897abe23
Jason Schwarz <jason.c.schwarz@gmail.com>
2025-12-10 17:32:38 -0400

    feat: minting flow

--- src/lib/contract-config.tsx
+++ src/lib/contract-config.tsx
@@ -1,9 +1,13 @@
 export const CONTRACT_ABI = [
     { "inputs": [], "name": "mint", "outputs": [], "stateMutability": "payable", "type": "function" },
-    { "inputs": [{ "internalType": "address", "name": "owner", "type": "address" }], "name": "balanceOf", "outputs": [{ "internalType": "uint256", "name": "", "type": "uint256" }], "stateMutability": "view", "type": "function" }
-]
+    { "inputs": [{ "internalType": "address", "name": "owner", "type": "address" }], "name": "balanceOf", "outputs": [{ "internalType": "uint256", "name": "", "type": "uint256" }], "stateMutability": "view", "type": "function" },
+    { "inputs": [], "name": "_mintPrice", "outputs": [{ "internalType": "uint256", "name": "", "type": "uint256" }], "stateMutability": "view", "type": "function" },
+    { "inputs": [{ "internalType": "address", "name": "user", "type": "address" }], "name": "getTokenIdByAddress", "outputs": [{ "internalType": "uint256", "name": "tokenId", "type": "uint256" }], "stateMutability": "view", "type": "function" },
+    {"inputs":[],"name":"_nftDescription","outputs":[{"internalType":"string","name":"","type":"string"}],"stateMutability":"view","type":"function"},
+    {"inputs":[],"name":"_nftName","outputs":[{"internalType":"string","name":"","type":"string"}],"stateMutability":"view","type":"function"},
+    {"inputs":[],"name":"_assetURI","outputs":[{"internalType":"string","name":"","type":"string"}],"stateMutability":"view","type":"function"}
+] as const;
 
-export const CONTRACT_ADDRESS = "";
-export const EXPLORER_URL = "https://etherscan.io/address/";
+export const CONTRACT_ADDRESS = "0xFAee8EDCDF7f7B2B56Ce78171714FC237f86ba7c";
 
 
