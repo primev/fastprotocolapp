097faaab6d2613c6ba4146e239befb1807ed31f6
Jason Schwarz <jason.c.schwarz@gmail.com>
2025-12-13 16:15:58 -0400

    refactor: remove mint price

--- src/lib/contract-config.tsx
+++ src/lib/contract-config.tsx
@@ -3,10 +3,9 @@ export const CONTRACT_ABI = [
     { "inputs": [{ "internalType": "address", "name": "user", "type": "address" }], "name": "getTokenIdByAddress", "outputs": [{ "internalType": "uint256", "name": "tokenId", "type": "uint256" }], "stateMutability": "view", "type": "function" },
 ] as const;
 
-export const CONTRACT_ADDRESS = "0x403D19503D257c5Fd759876C477f8442A82B8E37";
+export const CONTRACT_ADDRESS = "0xAf495aBD6B17B624Ea684E1f37cd1e3F471363B4";
 
 // Default data for the SBT
-export const MINT_PRICE = BigInt(0);
 export const NFT_NAME = 'Genesis SBT';
 export const NFT_DESCRIPTION = 'Your Genesis SBT proves you were early to Fast Protocol. Your progress will carry into the main Fast ecosystem at launch.';
 export const NFT_ASSET = '/assets/sbt-asset.png';
