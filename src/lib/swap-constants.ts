ec827c26633d93a35c930cf2260c7a6eb15767be
Jason Schwarz <jason.c.schwarz@gmail.com>
2026-01-19 17:06:24 -0400

    feat: inital swap setup

---
+++ src/lib/swap-constants.ts
@@ -0,0 +1,14 @@
+/**
+ * Constants for swap functionality
+ */
+
+/**
+ * Permit2 contract address (same on all chains)
+ */
+export const PERMIT2_ADDRESS = "0x000000000022D473030F116dDEE9F6B43aC78BA3" as const
+
+/**
+ * FastSettlementV3 contract address
+ * TODO: Replace with actual deployed address
+ */
+export const FAST_SETTLEMENT_ADDRESS = "0x0000000000000000000000000000000000000000" as const
